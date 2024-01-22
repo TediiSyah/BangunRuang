15c7d1848945804b737e09d12d6f7d2e20f7ce5a
TediiSyah <156158722+TediiSyah@users.noreply.github.com>
2024-01-22 19:06:52 +0700

    Add files via upload

---
+++ java/com/mycompany/bangunruang/LimasSegitiga.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bangunruang;
+
+/**
+ *
+ * @author Tedii
+ */
+public class LimasSegitiga {
+      double la,tl;
+   double volume(){
+   double volume = 1.0/3.0*la*tl;
+       System.out.println("Volume limas:"+volume+"cm3");
+       return volume;
+}
+}
